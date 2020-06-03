46e23421eb774fa6ef351ac50ea0c8b698dce15a
Maha Alrashed <mahafhr@gmail.com>
2020-06-03 23:38:40 +0300

    Update PS2.P2.js

--- PS2/PS2.P2.js
+++ PS2/PS2.P2.js
@@ -1,6 +1,6 @@
 /*
 PS2.P2.js - CS412 Summer 2020
-Write a generator that is initialized with a sentence and that emits each word of the sentence in
+A generator that is initialized with a sentence and that emits each word of the sentence in
 turn.
  */
 
