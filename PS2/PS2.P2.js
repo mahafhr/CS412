3df1cb9a4304914ab5f864d4d65fe1278fe36f8f
Maha Alrashed <mahafhr@gmail.com>
2020-06-03 23:38:14 +0300

    Update PS2.P2.js

--- PS2/PS2.P2.js
+++ PS2/PS2.P2.js
@@ -2,8 +2,6 @@
 PS2.P2.js - CS412 Summer 2020
 Write a generator that is initialized with a sentence and that emits each word of the sentence in
 turn.
-Use the generator to print the words, one per line, of the string “All I know is something like a
-bird within her sang”. Hint: Splitting a string returns an Array.
  */
 
 function* generateStr (str) {
